76e2c237ad344824040f5cdcf69665afd31e5e25
bensnider9 <bensnider9@gmail.com>
2025-10-01 18:24:41 -0400

    Update coin_toss.py

--- coin_toss.py
+++ coin_toss.py
@@ -1,14 +1,19 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Ben Snider
+# DATE: October 1, 2025
+# BRIEF DESCRIPTION: Coin Toss
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
 ########## ENTER YER CODE BELOW THIS LINE ##########
-
-
+import random
+print('===== Coin Flipper =====')
+num = random.randint(1,100)
+if 1<= num <=50:
+    print('Heads')
+else: 
+    print('tails')
 
 
 
@@ -43,8 +48,8 @@ Tails
 '''
 
 1. What was the hardest part of completing this lab? 
-
-
+The hardest pat of this lab for me was challenging myself to not look back into the notebook
+for the random portion.
 
 
 
@@ -63,5 +68,5 @@ may impede your understanding. Please rate how well you understand the concepts 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
 [ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
+[x ] I'm solid. Totally got it.
 '''
